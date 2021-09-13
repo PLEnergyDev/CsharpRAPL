a85eb70f9b80941fae258f12648f5176a3e4d645
Dreamer <freedomforce360@gmail.com>
2021-09-13 10:02:12 +0200

    feat: Adds test project
    feat: Adds tests for DeviceApi

---
+++ CsharpRAPLTests/DummyDevice.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace CsharpRAPL.Tests {
+	public class DummyDevice : DummyApi {
+		public override List<double> Collect() {
+			return new List<double> { 2514970.492 };
+		}
+
+		public override List<string> OpenRaplFiles() {
+			return new List<string>() { "/sys/class/powercap/intel-rapl:0/energy_uj", };
+		}
+	}
+}
