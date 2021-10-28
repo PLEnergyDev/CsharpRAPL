747b2d84f0dff433aa0703e56e05eb8f19604cb0
Lasse Rasmussen <freedomforce360@gmail.com>
2021-10-28 10:32:51 +0000

    Fix/fixes p values path not created

--- ExampleProject/Suite.cs
+++ ExampleProject/Suite.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.IO.Compression;
 using CsharpRAPL.Analysis;
 using CsharpRAPL.Benchmarking;
 using CsharpRAPL.CommandLine;
@@ -32,10 +33,18 @@ foreach ((string group, List<IBenchmark> benchmarks) in suite.GetBenchmarksByGro
 	Dictionary<string, double> result = Analysis.CalculatePValueForGroup(benchmarks);
 	DateTime dateTime = DateTime.Now;
 	var time = $"{dateTime.ToString("s").Replace(":", "-")}-{dateTime.Millisecond}";
+	Directory.CreateDirectory(Path.Join(options.OutputPath, $"_pvalues/{group}/"));
 	using var writer = new StreamWriter(Path.Join(options.OutputPath, $"_pvalues/{group}/{time}"));
 	using var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture) { Delimiter = ";" });
 	csv.WriteRecords(result);
 }
 
+
+using var zipToOpen = new FileStream("results.zip", FileMode.Open);
+using var archive = new ZipArchive(zipToOpen, ZipArchiveMode.Update);
+foreach (string file in Directory.EnumerateFiles(Path.Join(options.OutputPath, "_pvalues/"), "*.csv",
+	SearchOption.AllDirectories)) {
+	archive.CreateEntryFromFile(file, file);
+}
 
 CsharpRAPLCLI.StartAnalysis(suite.GetBenchmarksByGroup());
