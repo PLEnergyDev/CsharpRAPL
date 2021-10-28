3211ff7c69116dedfe90fd728f51cc390fede327
Milton Kristian Lindof <gitlab@miltonlindof.com>
2021-10-28 09:52:25 +0000

    Feat/write p value to files

--- ExampleProject/Suite.cs
+++ ExampleProject/Suite.cs
@@ -1,6 +1,12 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using CsharpRAPL.Analysis;
 using CsharpRAPL.Benchmarking;
 using CsharpRAPL.CommandLine;
+using CsvHelper;
+using CsvHelper.Configuration;
 
 CsharpRAPLCLI.SetAnalysis(analysis => {
 	Console.WriteLine($"Min:\n{analysis.GetMin()}");
@@ -21,5 +27,15 @@ var suite = new BenchmarkCollector(options.Iterations != -1 ? options.Iterations
 	options.LoopIterations != -1 ? options.LoopIterations : options.DefaultLoopIterations);
 
 suite.RunAll();
+
+foreach ((string group, List<IBenchmark> benchmarks) in suite.GetBenchmarksByGroup()) {
+	Dictionary<string, double> result = Analysis.CalculatePValueForGroup(benchmarks);
+	DateTime dateTime = DateTime.Now;
+	var time = $"{dateTime.ToString("s").Replace(":", "-")}-{dateTime.Millisecond}";
+	using var writer = new StreamWriter($"pvalues/{group}/{time}");
+	using var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture) { Delimiter = ";" });
+	csv.WriteRecords(result);
+}
+
 
 CsharpRAPLCLI.StartAnalysis(suite.GetBenchmarksByGroup());
