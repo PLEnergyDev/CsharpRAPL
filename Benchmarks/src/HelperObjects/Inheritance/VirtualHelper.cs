47a5aa99cc5018d6a0251a7c3a2ed6e58872da5b
Milton Kristian Lindof <gitlab@miltonlindof.com>
2021-12-13 13:33:44 +0000

    naming

--- Benchmarks/src/HelperObjects/Inheritance/VirtualHelper.cs
+++ Benchmarks/src/HelperObjects/Inheritance/VirtualHelper.cs
@@ -3,11 +3,11 @@
 namespace Benchmarks.HelperObjects.Inheritance;
 
 public class VirtualHelper {
-	private int _staticField = 4;
+	private int _field = 4;
 
 	[MethodImpl(MethodImplOptions.NoInlining)]
 	public virtual int UpdateAndGetValue() {
-		_staticField++;
-		return _staticField;
+		_field++;
+		return _field;
 	}
 }
