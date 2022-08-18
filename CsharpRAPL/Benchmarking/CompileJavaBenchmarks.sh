d90c758af4af24d45e29fcfb50610d8ad9a5c90a
Adrian Plesner <adrian.plesner@gmail.com>
2022-08-18 11:18:08 +0200

    Done: java benchmarks

--- CsharpRAPL/Benchmarking/CompileJavaBenchmarks.sh
+++ CsharpRAPL/Benchmarking/CompileJavaBenchmarks.sh
@@ -6,16 +6,19 @@ if [ "$comp" == "" ]; then
     echo "javac not found! Please install javac to continue" && exit
 fi 
 
+cd "$1" || exit
+
 echo -n "Compiling... "
 SUBSTRING=$(echo "$2"| cut -d'"' -f 2)
 if test "$SUBSTRING" != "" ; then
   echo -n "Compiler options: $SUBSTRING... "
+  javac *.java "$SUBSTRING"
 else
   echo -n "No additional compiler options found... "
+  javac *.java out
 fi
 
-javac *.java
-jar -cmf MANIFEST.MF JavaBench.jar JavaRun/*.class -d out "$SUBSTRING"
+jar -cmf MANIFEST.MF JavaBench.jar *.class 
 echo "java -jar JavaBench.jar \$1" > JavaBench.sh
 
 echo "Done!"
--- CsharpRAPL/Benchmarking/Lifecycles/JavaState.cs
+++ CsharpRAPL/Benchmarking/Lifecycles/JavaState.cs
@@ -15,36 +15,27 @@ public class JavaState : IpcState {
 
 	public override IpcState Generate() {
 		//Create directory and copy lib
-		string[] filesToCopy = { "JavaRun/Cmd.java", "JavaRun/FPipe.java","JavaRun/PipeCmdException.java", "MANIFEST.MF" };
+		string[] filesToCopy = { "Cmd.java", "FPipe.java","PipeCmdException.java", "MANIFEST.MF" };
 		var dt = DateTime.Now;
 		var dir = Directory.CreateDirectory(
 			$"tmp/JavaBench/{BenchmarkSignature}-{dt.ToString("s").Replace(":", "-")}-{dt.Millisecond}");
-		Directory.CreateDirectory(dir.FullName + "/JavaRun");
-		Directory.CreateDirectory(dir.FullName + "/out");
 		foreach (var f in filesToCopy) {
 			File.Copy($"{LibPath}/{f}",$"{dir.FullName}/{f}");
 		}
 		
 		// Write main file
-		var main = File.ReadAllLines(LibPath + "/JavaRun/Main.java").ToList();
+		var main = File.ReadAllLines(LibPath + "/Main.java").ToList();
 		var benchmarkLine = main.FindIndex(s => s.Contains("///Compute benchmark here"));
 		main[benchmarkLine] = BenchmarkSignature;
-		File.WriteAllLines(dir.FullName + "/JavaRun/Main.java", main);
+		File.WriteAllLines(dir.FullName + "/Main.java", main);
 		
 		// Write benchmark file
 		var bench = File.ReadAllLines($"{LibPath}/{JavaFile}").ToList();
-		var packageLine = bench.FindIndex(s => s.StartsWith("package"));
-		if (packageLine <= 0) {
-			bench[packageLine] = "package JavaRun;";
-		}
-		else {
-			bench.Insert(0, "package JavaRun;");
-		}
 		File.WriteAllLines($"{dir.FullName}/{JavaFile}", bench);
 		
 		//Run compile script
-		var compile = new ProcessStartInfo("/bin/bash") {
-			Arguments = "CompileJavaBenchmarks.sh " + dir.FullName + " \"" + AdditionalCompilerOptions + "\"",
+		var compile = new ProcessStartInfo("CompileJavaBenchmarks.sh") {
+			Arguments =  dir.FullName + " \"" + AdditionalCompilerOptions + "\"",
 			UseShellExecute = true,
 			CreateNoWindow = true
 		};
