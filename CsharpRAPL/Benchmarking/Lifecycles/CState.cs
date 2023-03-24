70df4a867286b17cbb4b1bbb62e89640ad7740b4
Adrian Plesner <adrian.plesner@gmail.com>
2023-03-24 14:32:54 +0100

    support for c libraries

--- CsharpRAPL/Benchmarking/Lifecycles/CState.cs
+++ CsharpRAPL/Benchmarking/Lifecycles/CState.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using Accord.Math;
 using SocketComm;
 
@@ -12,6 +14,27 @@ public class CState : IpcState {
 	public string CFile { get; set; }
 	public string HeaderFile { get; set; }
 	public string LibPath { get; set; }
+	private List<string> _libs = new List<string>{"libSocketComm.a"};
+	private List<string> _includes = new List<string>{"SocketComm"};
+	public List<string> Libs {
+		get => _libs;
+		set {
+		var result = new List<string>();
+		result.Add("libSocketComm.a");
+		result.AddRange(value);
+		_libs = result;
+		}
+	}
+
+	public List<string> Includes {
+		get => _includes;
+		set {
+			var result = new List<string>();
+			result.Add("SocketComm");
+			result.AddRange(value);
+			_includes = result;
+		}
+	}
 	
 	public string? AdditionalCompilerOptions { get; set; }
 
@@ -28,9 +51,21 @@ public class CState : IpcState {
 		}
 	}
 
+	private string _sendSignature;
+
+	public string SendSignature {
+		get => _sendSignature;
+		set {
+			_sendSignature = value;
+			if (!value.EndsWith(";")) {
+				_sendSignature += ";";
+			}
+		}
+	}
+
 	protected override IpcState Generate() {
 		//Create directories and copy lib
-		string[] filesToCopy = {"cmd.c","cmd.h","scomm.c","scomm.h", CFile, HeaderFile };
+		string[] filesToCopy = {CFile, HeaderFile };
 		var dt = DateTime.Now;
 		var dir= Directory.CreateDirectory($"tmp/CBench/{BenchmarkSignature}-{dt.ToString("s").Replace(":", "-")}-{dt.Millisecond}");
 		CompilationPath = dir.FullName;
@@ -38,17 +73,31 @@ public class CState : IpcState {
 			File.Copy($"{LibPath}/{f}",$"{dir.FullName}/{f}");
 		}
 		
+		//Add includes and libraries to compiler options
+		var libs = Path.GetFullPath(LibPath + "/lib");
+		var includes = Path.GetFullPath(LibPath + "/include");
+		AdditionalCompilerOptions = $"-L{libs} " + AdditionalCompilerOptions;
+		foreach (var include in Includes) {
+			AdditionalCompilerOptions = $"-I{includes}/{include} " + AdditionalCompilerOptions;
+		}
+		foreach (var lib in Libs) {
+			AdditionalCompilerOptions += $" -l:{lib}";
+		}
+
 		//Write main file
-		var main = File.ReadAllLines(LibPath + "/main.c");
-		var includeLine = main.First(s => s.Contains("///Includes here"));
-		var benchmarkLine = main.First(s => s.Contains("///Compute benchmark here"));
-		main[benchmarkLine] = BenchmarkSignature;
-		main[includeLine] = $"#include \"{HeaderFile}\"";
-		File.WriteAllLines(dir.FullName + "/main.c", main);
+		var mainFile = File.ReadAllLines(LibPath + "/main.c");
+
+		var includeLine = mainFile.First(s => s.Contains("///Includes here"));
+		var benchmarkLine = mainFile.First(s => s.Contains("///Compute benchmark here"));
+		var sendLine = mainFile.First(s => s.Contains("///Send return value here"));
+		mainFile[benchmarkLine] = BenchmarkSignature;
+		mainFile[includeLine] = $"#include \"{HeaderFile}\"";
+		mainFile[sendLine] = SendSignature;
+		File.WriteAllLines(dir.FullName + "/main.c", mainFile);
 		
 		//Run compile script
 		var compile = new ProcessStartInfo("CompileCBenchmarks.sh");
-		compile.Arguments = $"{dir.FullName} {CFile} \"{AdditionalCompilerOptions}\"";
+		compile.Arguments = $"\"{dir.FullName}\" {CFile} \"{AdditionalCompilerOptions}\"";
 		compile.CreateNoWindow = true;
 		compile.UseShellExecute = true;
 		var compP = Process.Start(compile);
